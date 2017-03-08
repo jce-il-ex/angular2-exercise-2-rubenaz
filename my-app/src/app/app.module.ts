60163f7ba3bc4c96da18df47ea32feac4a4fde51
Ruben <Ruben>
2017-03-08 13:17:41 +0200

    c

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,16 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { CmpntComponent } from './cmpnt/cmpnt.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { NavItemComponent } from './nav-item/nav-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CmpntComponent,
-    CmpntComponent,
-    CmpntComponent,
     NavMenuComponent,
     NavItemComponent
   ],
