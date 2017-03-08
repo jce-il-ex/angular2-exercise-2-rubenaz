cb3b74a352feae1abdd2d7cc0f13a8dda8c095c4
Ruben <Ruben>
2017-03-08 14:06:00 +0200

    c

--- my-app/src/app/nav-item/nav-item.component.ts
+++ my-app/src/app/nav-item/nav-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-nav-item',
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavItemComponent implements OnInit {
 
   constructor() { }
-
+  @Input() item ;
   ngOnInit() {
   }
 
--- my-app/src/app/nav-menu/nav-menu.component.ts
+++ my-app/src/app/nav-menu/nav-menu.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavMenuComponent implements OnInit {
 
   constructor() { }
-
+  navArray = [{ id:"home"}, {id:"Option"},{id:"instruction"},{ id:"about"}];
   ngOnInit() {
   }
 
